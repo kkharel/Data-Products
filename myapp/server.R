155f7f3adc9453e96c64c7f10f299dfd0957335c
kkharel <kkharel@uci.edu>
2022-08-21 15:01:50 -0700

    Progress

--- myapp/server.R
+++ myapp/server.R
@@ -1,19 +1,171 @@
 library(shiny)
+#
+# shinyServer(function(input, output) {
+#
+#   output$plot1 = renderPlot({
+#     set.seed(2016-05-25)
+#     number_of_points = input$numeric
+#     minX = input$sliderX[1]
+#     maxX = input$sliderX[2]
+#     minY = input$sliderY[1]
+#     maxY = input$sliderY[2]
+#     dataX = runif(number_of_points, minX, maxX)
+#     dataY = runif(number_of_points, minY , maxY)
+#     xlab = ifelse(input$show_xlab, "X Axis", "")
+#     ylab = ifelse(input$show_ylab, "Y Axis", "")
+#     main = ifelse(input$show_title, "Title", "")
+#     plot(dataX, dataY, xlab = xlab, ylab= ylab, main = main, xlim = c(-100,100), ylim = c(-100,100))
+#   })
+# })
 
-shinyServer(function(input, output) {
 
-  output$plot1 = renderPlot({
-    set.seed(2016-05-25)
-    number_of_points = input$numeric
-    minX = input$sliderX[1]
-    maxX = input$sliderX[2]
-    minY = input$sliderY[1]
-    maxY = input$sliderY[2]
-    dataX = runif(number_of_points, minX, maxX)
-    dataY = runif(number_of_points, minY , maxY)
-    xlab = ifelse(input$show_xlab, "X Axis", "")
-    ylab = ifelse(input$show_ylab, "Y Axis", "")
-    main = ifelse(input$show_title, "Title", "")
-    plot(dataX, dataY, xlab = xlab, ylab= ylab, main = main, xlim = c(-100,100), ylim = c(-100,100))
-  })
-})
+# diabetesRisk = function(glucose) glucose/200
+#
+# shinyServer(
+#   function(input, output) {
+#     output$inputValue = renderPrint({input$glucose})
+#     output$prediction = renderPrint({diabetesRisk(input$glucose)})
+#   }
+# )
+#
+# # prediction function is defined outside the server function. The server function takes in the input
+# # and repeats both the input glucose level and the outputted diabetes risk
+
+# library(usingR)
+
+# library(HistData)
+#
+#
+# data(Galton)
+#
+# shinyServer(
+#   function(input, output) {
+#     output$newHist = renderPlot({
+#       hist(Galton$child, xlab = 'child height', col = 'blue', main = 'Histogram')
+#       mu = input$mu
+#       lines(c(mu,mu), c(0,200), col = 'red', lwd = 5)
+#       mse = mean((Galton$child - mu)^2)
+#       text(63, 150, paste("mu = ", mu))
+#       text(63, 130, paste("MSE = ", round(mse,2)))
+#     })
+#   }
+# )
+#
+#
+
+#
+# library(shiny)
+#
+# shinyServer(
+#   function(input, output) {
+#     mtcars$mpgsp = ifelse(mtcars$mpg - 20 > 0, mtcars$mpg - 20, 0) # this is just spline term. If the miles per gallon of the car is larger than 20 then return miles per gallon, otherwise return zero
+#     model1 = lm(hp ~ mpg, data = mtcars)
+#     model2 = lm(hp ~ mpgsp + mpg, data = mtcars)
+#
+#     model1pred = reactive({
+#       mpgInput = input$sliderMPG
+#       predict(model1, newdata = data.frame(mpg = mpgInput))
+#     })
+#
+#     model2pred = reactive({
+#       mpgInput = input$sliderMPG
+#       predict(model2, newdata = data.frame(mpg = mpgInput,
+#                                            mpgsp = ifelse(mpgInput - 20 > 0,
+#                                                           mpgInput - 20 , 0)))
+#     })
+#       output$plot1 = renderPlot({
+#       mpgInput = input$sliderMPG
+#
+#       plot(mtcars$mpg, mtcars$hp, xlab = "Miles Per Gallon",
+#            ylab = "Horsepower", bty = "n", pch = 16,
+#            xlim = c(10, 35), ylim = c(50,350))
+#       if(input$showModel1) {
+#         abline(model1, col = "red", lwd = 2) # adding the fit for model 1
+#       }
+#       if(input$showModel2) {
+#         model2lines = predict(model2, newdata = data.frame(mpg = 10:35,
+#                                                            mpgsp = ifelse(10:35 - 20 > 0,
+#                                                                           10:35 - 20,
+#                                                                           0))) # if model 2, add fit for model 2
+#         lines(10:35, model2lines, col = "blue", lwd = 2)
+#       }
+#       legend(25,250, c("Model 1 Prediction", "Model 2 Prediction"), pch = 16,
+#              col = c("red", "blue"), bty = "n", cex = 1.2)
+#       points(mpgInput, model1pred(), col = "red", pch = 16, cex = 2)
+#       points(mpgInput, model2pred(), col = "blue", pch = 16, cex = 2)
+#     })
+#
+#       output$pred1 = renderText({
+#         model1pred()
+#       })
+#
+#       output$pred2 = renderText({
+#         model2pred()
+#       })
+#   }
+# )
+
+#
+# Delayed Reactivity
+#
+# We might not want out app to immediately react to changes in user input because
+# of something like a long-running calculation. In order to prevent reactive expressions
+# from reacting we can use a submit button in our app.
+
+# tabs
+
+# library(shiny)
+#
+# shinyServer(
+#   function(input, output) {
+#     output$out1 = renderText(input$box1)
+#     output$out2 = renderText(input$box2)
+#     output$out3 = renderText(input$box3)
+#   }
+# )
+
+# Interactive Graphics
+
+# data(trees)
+#
+# library(shiny)
+#
+# shinyServer(
+#   function(input, output) {
+#     model = reactive({
+#       brushed_data = brushedPoints(trees, input$brush1,
+#                                    xvar = "Girth", yvar = "Volume")
+#
+#       if(nrow(brushed_data) < 2){ #if fewer than two points then return null
+#         return(NULL)
+#       }
+#       lm(Volume ~ Girth, data = brushed_data) #otherwise fit and return linear model
+#     })
+#     output$slopeOut = renderText({
+#       if(is.null(model())){
+#         "No Model Found"
+#       } else {
+#         model()[[1]][2]
+#       }
+#     })
+#     output$intOut = renderText({
+#       if(is.null(model())){
+#         "No Model Found"
+#       } else {
+#         model()[[1]][1]
+#       }
+#     })
+#     output$plot1  = renderPlot({
+#       plot(trees$Girth, trees$Volume, xlab = "Girth",
+#            ylab = "Volume", main = "Tree Measurements",
+#            cex = 1.5, pch = 16, bty = "n")
+#       if(!is.null(model())){ # if the model is not null then
+#         abline(model(), col = "blue", lwd = 2) # add a line that is only for those specific grabbed data points
+#       }
+#     })
+#   }
+# )
+
+
+# Shiny Gadgets
+
--- myapp/ui.R
+++ myapp/ui.R
@@ -1,25 +1,25 @@
-library(shiny)
-
-shinyUI(fluidPage(
-  titlePanel("Plot Random Numbers"),
-  sidebarLayout(
-    sidebarPanel(
-      numericInput("numeric", "How many random numbers should be plotted?",
-                   value = 1000, min = 1, max = 1000, step = 1),
-      sliderInput("sliderX", "Pick Minimum and Maximum X values",
-                  -100, 100, value = c(-50, 50)),
-      sliderInput("sliderY", "Pick Minimum and Maximum Y values",
-                  -100, 100, value = c(-50, 50)),
-      checkboxInput("show_xlab", "Show/Hide X Axis Label", value = TRUE),
-      checkboxInput("show_ylab", "Show/Hide Y Axis Label", value = TRUE),
-      checkboxInput("show_title", "Show/Hide Title")
-    ),
-    mainPanel(
-      h1("Graph of Random Points"),
-      plotOutput("plot1")
-    )
-  )
-))
+# library(shiny)
+#
+# shinyUI(fluidPage(
+#   titlePanel("Plot Random Numbers"),
+#   sidebarLayout(
+#     sidebarPanel(
+#       numericInput("numeric", "How many random numbers should be plotted?",
+#                    value = 1000, min = 1, max = 1000, step = 1),
+#       sliderInput("sliderX", "Pick Minimum and Maximum X values",
+#                   -100, 100, value = c(-50, 50)),
+#       sliderInput("sliderY", "Pick Minimum and Maximum Y values",
+#                   -100, 100, value = c(-50, 50)),
+#       checkboxInput("show_xlab", "Show/Hide X Axis Label", value = TRUE),
+#       checkboxInput("show_ylab", "Show/Hide Y Axis Label", value = TRUE),
+#       checkboxInput("show_title", "Show/Hide Title")
+#     ),
+#     mainPanel(
+#       h1("Graph of Random Points"),
+#       plotOutput("plot1")
+#     )
+#   )
+# ))
 
 
 # Reactivity
@@ -36,5 +36,221 @@ shinyUI(fluidPage(
 # })
 #
 # calc_sum()
+
+
+# Prediction Algorithm for Diabetes
+
+
+# shinyUI(
+#   pageWithSidebar(
+#     # Application Title
+#     headerPanel("Diabetes prediction"),
+#     sidebarPanel(
+#       numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5),
+#       submitButton('Submit')
+#     ),
+#     mainPanel(
+#       h3('Results of prediction'),
+#       h4('You entered'),
+#       verbatimTextOutput("inputValue"),
+#       h4('Which resulted in a prediction of '),
+#       verbatimTextOutput("prediction") # function to display the output of our server.R functions
+#     )
+#   )
+# )
+
+
+#Galton Data
+
+# shinyUI(
+#   pageWithSidebar(
+#     headerPanel("Example Plot"),
+#     sidebarPanel(
+#       sliderInput('mu', 'Guess at the mean', value = 70, min = 62, max = 74, step = 0.05)
+#     ),
+#     mainPanel(
+#       plotOutput('newHist')
+#     )
+#   )
+# )
+#
+
+# reactive expression
+
+# library(shiny)
+#
+# shinyUI(
+#   fluidPage(
+#     titlePanel("Predict Horsepower from MPG"),
+#     sidebarLayout(
+#       sidebarPanel(
+#         sliderInput("sliderMPG", "What is the MPG of the car?", 10, 35, value = 20), # range from 10 miles per gallon to 35 and default value of 20
+#         checkboxInput("showModel1", "Show/Hide Model 1", value = TRUE),
+#         checkboxInput("showModel2", "Show`/Hide Model 2", value = TRUE),
+#         submitButton("Submit") # delayed reactive
+#       ),
+#       mainPanel(
+#         plotOutput("plot1"),
+#         h3("Predicted Horesepower from Model 1:"),
+#         textOutput("pred1"),
+#         h3("Predicted Horsepower from model 2:"),
+#         textOutput("pred2")
+#       )
+#     )
+#   )
+# )
+
+
+# Advanced UI
+#
+# We can include different UI mix into our app including tabls, navbars, and sidebars.
+# We can use tab to have multiple views in our app.
+# The tabsetPanel() function specifies a group of tabs and then the tabPanel() function
+# specifies the contents of an individual tab.
+
+
+# library(shiny)
+#
+# shinyUI(
+#   fluidPage(
+#     titlePanel("Tabs!"),
+#     sidebarLayout(
+#       sidebarPanel(
+#         textInput("box1", "Enter Tab 1 Text:", value = "Tab 1!"),
+#         textInput("box2", "Enter Tab 2 Text:", value = "Tab 2!"),
+#         textInput("box3", "Enter Tab 3 Text:", value = "Tab 3!")
+#       ),
+#       mainPanel(
+#         tabsetPanel(type = "tabs",
+#                     tabPanel("Tab 1", br(), textOutput("out1")),
+#                     tabPanel("Tab 2", br(), textOutput("out2")),
+#                     tabPanel("Tab 3", br(), textOutput("out3"))
+#                     )
+#       )
+#     )
+#   )
+# )
+
+
+# Interactive Graphics
+
+# Shiny can create graphics that users can interact with.
+# One method we can use to select multiple data points on the graph
+# is by using brush argument in plotOutput() on ui.R and then using the
+# brushedPoints() function on the server.R
+#
+# example:
+#
+# Fitting a linear model for the selected points and then drawing a line of best fit for the resulting model.
+
+
+# library(shiny)
+#
+# shinyUI(
+#   fluidPage(
+#     titlePanel("Visualize Many Models"),
+#     sidebarLayout(
+#       sidebarPanel(
+#         h3("Slope"),
+#         textOutput("slopeOut"),
+#         h3("Intercept"),
+#         textOutput("intOut")
+#       ),
+#       mainPanel(
+#         plotOutput("plot1", brush = brushOpts(
+#           id = "brush1"
+#         ))
+#       )
+#     )
+#   )
+# )
+
+# Shiny Gadgets
+
+# provides a way to use Shiny's interactivity and user interface tools as a part of
+# data analysis. With Shiny Gadgets, we can create a function that opens a small Shiny app.
+# Since these apps are smaller we will be using miniUI package for creating
+# user interfaces
+
+# At its core Shiny Gadget is a function that launches a small (single-page) shiny application.
+# Since shiny gadgets are meant to be displayed in the RStudio viewer pane, the miniUI package
+# comes in handy for its smaller graphical elements.
+
+# Constructing a simple Shiny Gadget
+
+# library(shiny)
+# library(miniUI)
+#
+# myFirstGadget = function() {
+#   ui = miniPage(
+#     gadgetTitleBar("My first Gadget")
+#   )
+#   server = function(input, output, session) {
+#     # Done button closes the app
+#     observeEvent(input$done, {
+#       stopApp()
+#     })
+#   }
+#   runGadget(ui, server)
+# }
+
+
+# One of the advantages of Shiny Gadgets is that since Shiny Gadgets are functions, they
+# can take values as arguments and they can return values.
+# The following shiny Gadget takes two different vectors of numbers as arguments
+# and uses those vectors to populate two selectInputs.
+# The user can then choose two numbers within the Gadget, and the product of those two numbers
+# will be returned
+
+
+# library(shiny)
+# library(miniUI)
+#
+# multiplyNumbers = function(numbers1, numbers2) {
+#   ui = miniPage(
+#     gadgetTitleBar("Multiply Two Numbers"),
+#     miniContentPanel(
+#       selectInput("num1", "First Number", choices = numbers1),
+#       selectInput("num2", "Seconf Number", choices = numbers2)
+#     )
+#   )
+#   server = function(input, output, session) {
+#     observeEvent(input$done, {
+#       num1 = as.numeric(input$num1)
+#       num2 = as.numeric(input$num2)
+#       stopApp(num1 * num2)
+#     })
+#   }
+#   runGadget(ui,server)
+# }
+
+
+# Gadgets with Interactive Graphics
+
+# library(shiny)
+# library(miniUI)
+#
+# pickTrees = function() {
+#   ui = miniPage(
+#     gadgetTitleBar("Select Points by Dragging your Mouse"),
+#     miniContentPanel(
+#       plotOutput("plot", height = "100%", brush = "brush")
+#     )
+#   )
+#   server = function(input, output, session) {
+#     output$plot = renderPlot({
+#       plot(trees$Girth, trees$Volume, main = "Trees!",
+#            xlab = "Girth", ylab = "Volume")
+#     })
+#     observeEvent(input$done, {
+#       stopApp(brushedPoints(trees, input$brush,
+#                             xvar = "Girth", yvar = "Volume"))
+#     })
+#   }
+#   runGadget(ui, server)
+# }
+
+
+# Google Vis API
 
 
